1b6d08a819e3f312841ffb9159c74f045328ff33
PereiraJP01 <pereirajp01games@gmail.com>
2021-01-15 18:09:27 +0100

    Update db_connect.php

--- htmlhelper/db_connect.php
+++ htmlhelper/db_connect.php
@@ -1,13 +1,8 @@
 <?php 
 //conexão com base de dados 
 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$db_name = "gesticionario";
-
-
-
-$connect = mysqli_connect($servername, $username, $password, $db_name);
-
+$connect = pg_connect("host=ec2-54-155-99-116.eu-west-1.compute.amazonaws.com port=5432 dbname=d4da64p5bvskhi user=htdztxdjrfiewr password=415319eb91b02a1858b5929bdd7b621fcf83097a5315cd6123bf778318ac139c");
+if (!$connect) {  
+ echo "An error occurred.\n";  
+ exit;  
 mysqli_set_charset($connect, "utf8");
