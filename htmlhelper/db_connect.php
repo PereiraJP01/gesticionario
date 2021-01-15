7d86393732b17b2f2dbb90ff14eeda925688358b
PereiraJP01 <pereirajp01games@gmail.com>
2021-01-15 18:43:04 +0100

    Update db_connect.php

--- htmlhelper/db_connect.php
+++ htmlhelper/db_connect.php
@@ -8,4 +8,10 @@ $password = "415319eb91b02a1858b5929bdd7b621fcf83097a5315cd6123bf778318ac139c";
 
 $connect=mysqli_connect($host, $user, $password, $dbname);
 
+if($connect){
+  echo "SUCESSO!";
+}else{
+ echo "Falha";
+}
+
 mysqli_set_charset($connect, "utf8");
